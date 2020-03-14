330c708dbe78ed45e585415467c034abb4461aca
Austen Turner <austenpturner@msn.com>
2020-03-13 17:38:14 -0700

    add media queries

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
-    database: ""
+    password: "Apt0718!",
+    database: "burgers_db"
 });
 
 // Connect to mysql database
